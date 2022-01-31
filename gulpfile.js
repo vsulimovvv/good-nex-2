f2b45d959f6ce4907664fe0da6757bea115170eb
vsulimovvv <super.sulimov2014@yandex.ru>
2022-01-31 17:08:11 +0300

    edit

--- gulpfile.js
+++ gulpfile.js
@@ -79,7 +79,6 @@ function styles() {
 function scripts() {
   return src([
       'node_modules/jquery/dist/jquery.js',
-      // 'node_modules/simplebar/dist/simplebar.js',
       'node_modules/nouislider/dist/nouislider.js',
       'node_modules/mixitup/dist/mixitup.js',
       'node_modules/swiper/swiper-bundle.min.js',
@@ -98,7 +97,7 @@ function images() {
         interlaced: true
       }),
       imagemin.mozjpeg({
-        quality: 65,
+        quality: 85,
         progressive: true
       }),
       imagemin.optipng({
